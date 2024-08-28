df76eb92703343aae8b203907bb5699eb90dce58
24LP8078 24LP8078 <24LP8078@G305-CC0038.local>
2024-08-28 09:21:00 +0800

    delete

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-export const Button = (props) => {
-  const { text } = props;
-  return (
-    <div className="py-1 px-2 bg-blue-500 rounded-md text-[white] w-fit">
-      <button>{text}</button>
-    </div>
-  );
-};
